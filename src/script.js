0d030a5aca8af490a178ebc41b5290dba05e3f7a
Ravi Ranjan Ojha <raviranjanojha777@gmail.com>
2021-11-20 15:39:51 +0530

    Modified initial commit

--- src/script.js
+++ src/script.js
@@ -1,40 +1,56 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 let positions, x1, x2, x3, count;
 const canvas = document.querySelector('canvas.webgl');
 document.getElementById('create').addEventListener('click', createObject);
 document.getElementById('delete').addEventListener('click', () => {
-  while (scene.children.length > 0) {
-    scene.remove(scene.children[0]);
+  for (let i = 3; i > 1; i--) {
+    scene.remove(scene.children[i]);
   }
   points();
 });
 
 // Scene
 const scene = new THREE.Scene();
+var grid = new THREE.GridHelper(1000, 100);
+scene.add(grid);
 
 /**
  * Object
  */
 function createObject() {
-  console.log(count);
+  //   console.log(rgb);
   console.log(positions);
 
   const geometry = new THREE.BufferGeometry();
   const positionsArray = positions.slice();
-  positionsArray[count * 3 + 0] = x1;
-  positionsArray[count * 3 + 1] = x2;
-  positionsArray[count * 3 + 2] = x3;
+  //   positionsArray[count * 3 + 0] = x1;
+  //   positionsArray[count * 3 + 1] = x2;
+  //   positionsArray[count * 3 + 2] = x3;
   console.log(positionsArray);
   const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
   geometry.setAttribute('position', positionsAttribute);
 
+  geometry.setIndex([
+    0, 1, 2, 0, 2, 3, 0, 4, 5, 0, 6, 7, 0, 8, 9, 0, 10, 11, 0, 12, 13, 0, 14,
+    15, 0, 16, 17, 0, 18, 19, 0, 20, 21, 0, 22, 23, 0, 24, 25, 0, 26, 27, 0, 28,
+    29, 0, 30, 31, 0, 32, 33, 0, 34, 35, 0, 36, 37, 0, 38, 39, 0, 40, 41
+  ]);
+
   const material = new THREE.MeshBasicMaterial({
     color: 0xffa500
+    // wireframe: true
   });
 
   const mesh = new THREE.Mesh(geometry, material);
+  let lineMaterial = new THREE.LineBasicMaterial({
+    color: 0x000000,
+    linewidth: 2
+  });
+  let edges = new THREE.LineSegments(geometry, material);
+  mesh.add(edges);
   scene.add(mesh);
 }
 
@@ -68,9 +84,12 @@ const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
-  100
+  500
 );
-camera.position.z = 3;
+camera.position.set(0, 3, 2); // Set position like this
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+camera.position.y = 200;
+
 scene.add(camera);
 /////////////////////////////////////////////////
 function points() {
@@ -123,9 +142,11 @@ function points() {
       x2 = mouse.y;
       x3 = mouse.z;
     }
-    positions[count * 3 + 0] = mouse.x;
-    positions[count * 3 + 1] = mouse.y;
-    positions[count * 3 + 2] = mouse.z;
+    if (count !== 0) {
+      positions[count * 3 + 0] = mouse.x;
+      positions[count * 3 + 1] = mouse.y;
+      positions[count * 3 + 2] = mouse.z;
+    }
     count++;
     line.geometry.setDrawRange(0, count);
     updateLine();
@@ -149,8 +170,10 @@ controls.enableDamping = true;
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  alpha: true
 });
+renderer.setClearColor(0xffffff, 0);
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
